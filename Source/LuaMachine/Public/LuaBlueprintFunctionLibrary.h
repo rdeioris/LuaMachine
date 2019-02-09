d4c360b82020e70ad23892a2531ed2d8a3fd8972
Roberto De Ioris <roberto.deioris@gmail.com>
2019-02-09 19:39:32 +0100

    added doc/comment for LuaRunURL

--- Source/LuaMachine/Public/LuaBlueprintFunctionLibrary.h
+++ Source/LuaMachine/Public/LuaBlueprintFunctionLibrary.h
@@ -147,6 +147,7 @@ public:
 	UFUNCTION(BlueprintCallable, meta = (WorldContext = "WorldContextObject"), Category="Lua")
 	static FLuaValue LuaRunString(UObject* WorldContextObject, TSubclassOf<ULuaState> State, FString CodeString);
 
+	/* Make an HTTP GET request to the specified URL to download the Lua script to run */
 	UFUNCTION(BlueprintCallable, meta = (WorldContext = "WorldContextObject", AutoCreateRefTerm = "Headers"), Category = "Lua")
 	static void LuaRunURL(UObject* WorldContextObject, TSubclassOf<ULuaState> State, FString URL, TMap<FString, FString> Headers, FLuaHttpSuccess Completed);
 
